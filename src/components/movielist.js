161ff1c86b35fb65f5130adfe65659c9c1266785
Doge in trench coat <124916318+Cassius-K@users.noreply.github.com>
2026-04-19 22:43:23 -0600

    formatting

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -35,22 +35,41 @@ function MovieList() {
         <Carousel onSelect={handleSelect} className="bg-dark text-light p-4 rounded">
           {memoizedMovies.map((movie) => (
             <Carousel.Item key={movie._id}>
-              {/* Use Nav.Link with "as={Link}" to avoid nested anchors */}
               <Nav.Link
                 as={Link}
                 to={`/movie/${movie._id}`}
                 onClick={() => handleClick(movie)}
               >
-                <Image className="image" src={movie.imageUrl} thumbnail />
+                {}
+                <div style={{ height: '500px', backgroundColor: '#222', display: 'flex' }}>
+                    <img 
+                        className="d-block w-100" 
+                        src={movie.imageUrl} 
+                        alt={movie.title} 
+                        style={{ 
+                            maxHeight: '100%', 
+                            objectFit: 'contain' 
+                        }} 
+                    />
+                </div>
               </Nav.Link>
-              <Carousel.Caption>
+              
+              {}
+              <Carousel.Caption style={{ 
+                  backgroundColor: 'rgba(0, 0, 0, 0.75)', 
+                  borderRadius: '10px',
+                  padding: '10px',
+                  marginBottom: '20px' 
+              }}>
                 <h3>{movie.title}</h3>
-                <BsStarFill /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+                <p>
+                    {}
+                    <BsStarFill className="text-warning" /> {movie.avgRating ? movie.avgRating.toFixed(1) : 'N/A'} &nbsp;&nbsp;|&nbsp;&nbsp; {movie.releaseDate}
+                </p>
               </Carousel.Caption>
             </Carousel.Item>
           ))}
         </Carousel>
       );
-    }
 
 export default MovieList;
