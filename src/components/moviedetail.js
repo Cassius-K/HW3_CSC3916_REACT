42c710a900e56c8ecc0256de7a80f60c079b9e47
Doge in trench coat <124916318+Cassius-K@users.noreply.github.com>
2026-04-19 22:13:59 -0600

    redid movie detail to prevent crashing

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -3,68 +3,81 @@ import { fetchMovie } from '../actions/movieActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs';
-import { useParams } from 'react-router-dom'; // Import useParams
+import { useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
   const dispatch = useDispatch();
-  const { movieId } = useParams(); // Get movieId from URL parameters
+  const { movieId } = useParams(); 
+  
   const selectedMovie = useSelector(state => state.movie.selectedMovie);
-  const loading = useSelector(state => state.movie.loading); // Assuming you have a loading state in your reducer
-  const error = useSelector(state => state.movie.error); // Assuming you have an error state in your reducer
-
+  const loading = useSelector(state => state.movie.loading); 
+  const error = useSelector(state => state.movie.error); 
 
   useEffect(() => {
-    dispatch(fetchMovie(movieId));
+    if (movieId) {
+      dispatch(fetchMovie(movieId));
+    }
   }, [dispatch, movieId]);
 
-  const DetailInfo = () => {
-    if (loading) {
-      return <div>Loading....</div>;
-    }
+  if (loading) {
+    return <div>Loading....</div>;
+  }
 
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
-    if (!selectedMovie) {
-      return <div>No movie data available.</div>;
-    }
+  if (!selectedMovie) {
+    return <div>No movie data available.</div>;
+  }
 
-    return (
-      <Card className="bg-dark text-dark p-4 rounded">
-        <Card.Header>Movie Detail</Card.Header>
-        <Card.Body>
-          <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-        </Card.Body>
-        <ListGroup>
-          <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-          <ListGroupItem>
-            {selectedMovie.actors.map((actor, i) => (
-              <p key={i}>
-                <b>{actor.actorName}</b> {actor.characterName}
-              </p>
-            ))}
-          </ListGroupItem>
-          <ListGroupItem>
-            <h4>
-              <BsStarFill /> {selectedMovie.avgRating}
-            </h4>
-          </ListGroupItem>
-        </ListGroup>
-        <Card.Body className="card-body bg-white">
-          {selectedMovie.reviews.map((review, i) => (
+  return (
+    <Card className="bg-dark text-dark p-4 rounded">
+      <Card.Header>Movie Detail</Card.Header>
+      
+      <Card.Body>
+        {/* Added safety check in case imageUrl is missing */}
+        {selectedMovie.imageUrl && (
+            <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+        )}
+      </Card.Body>
+
+      <ListGroup>
+        <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+        
+        <ListGroupItem>
+          {}
+          {selectedMovie.actors?.map((actor, i) => (
+            <p key={i}>
+              <b>{actor.actorName}</b> as {actor.characterName}
+            </p>
+          ))}
+        </ListGroupItem>
+
+        <ListGroupItem>
+          <h4>
+            {/* Added fallback in case avgRating is null/undefined */}
+            <BsStarFill /> {selectedMovie.avgRating ? selectedMovie.avgRating.toFixed(1) : 'No Rating'}
+          </h4>
+        </ListGroupItem>
+      </ListGroup>
+
+      <Card.Body className="card-body bg-white">
+        <h3>Reviews</h3>
+        {}
+        {selectedMovie.reviews && selectedMovie.reviews.length > 0 ? (
+          selectedMovie.reviews.map((review, i) => (
             <p key={i}>
               <b>{review.username}</b>&nbsp; {review.review} &nbsp; <BsStarFill />{' '}
               {review.rating}
             </p>
-          ))}
-        </Card.Body>
-      </Card>
-    );
-  };
-
-  return <DetailInfo />;
+          ))
+        ) : (
+          <p>No reviews yet.</p>
+        )}
+      </Card.Body>
+    </Card>
+  );
 };
-
 
 export default MovieDetail;
